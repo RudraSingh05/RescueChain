7911e9fc0b3f678527416b455b6c68635070aaa5
Rudra Pratap Singh <144825212+RudraSingh05@users.noreply.github.com>
2026-02-27 03:27:18 +0530

    Add protected route and health check endpoint

--- services/auth-service/index.js
+++ services/auth-service/index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-require("dotenv").config();
 
 const authRoutes = require("./src/routes/auth.routes");
+const { authenticate } = require("./src/middleware/auth.middleware");
+
 
 const app = express();
 
@@ -12,5 +14,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.get("/protected", authenticate, (req, res) => {
+  res.json({ message: "Protected route accessed", user: req.user });
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "Auth Service Running ✅" });
+});
 
 module.exports = app;
